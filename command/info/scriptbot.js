1bfba04639e9244a56bdc9d2004e64b585b358e0
Zeetsuꭙࣼīn <107705029+ZeetsuShin@users.noreply.github.com>
2022-06-17 23:39:26 +0700

    New

--- command/info/scriptbot.js
+++ command/info/scriptbot.js
@@ -4,8 +4,9 @@ module.exports = {
 	category: "info",
 	isSpam: true,
 	async run({ msg, conn }, { q, map, args }) {
-	  text = `Script Bot : https://github.com/Rizky878/rzky-multidevice/\n\n`
-	  text += "Dont Forget to forks n star XD"
+	  text = `Script Bot : https://github.com/Rizky878/rzky-multidevice/\n`
+	  text += `Recode : https://github.com/ZeetsuShin/Bot-MD\n\n`
+          text += "Dont Forget to forks n star XD"
 	  await msg.reply("```" + text + "```");
 	},
 };
